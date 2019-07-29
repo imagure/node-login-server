9d61a0763e6e3345aef6c4599ceb60160d1f4585
RicardoImagure <ricardo.imagure@gmail.com>
2019-07-29 13:41:33 -0300

    Small changes to server

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const koa = require('koa');
 const routes = require("./routes/routes").router
-const cors = require('koa-cors');
+const cors = require('koa2-cors');
 
 const app = new koa();
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  allowMethods: ['POST']
+}));
+
 app.use(routes.routes());
 
 const server = app.listen(process.env.PORT || 4000, function(){
